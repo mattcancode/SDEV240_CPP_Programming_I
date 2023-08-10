68190935c2cebb8a7b4e732e19ad405ae26ae631
Matt Miller <matt@mattcancode.com>
2023-08-10 10:18:45 -0400

    adding week1 code and screenshot

---
+++ week1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    int firstNumber = 30;
+    int secondNumber = 12;
+
+    std::cout << firstNumber << " + " << secondNumber
+              << " = " << firstNumber + secondNumber
+              << std::endl;
+
+    return 0;
+}
